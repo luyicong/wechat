76016a190ac15f68244093f47e23ee74c36a58b6
lyc123 <980469887@qq.com>
2018-03-20 17:29:30 +0800

    。。。。

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+let _this
 Page({
   data: {
     imgUrls: [
@@ -14,7 +14,8 @@ Page({
     interval: 2000,
     duration: 800,
     indicatColor:'rgba(0,0,0,0.4)',
-    indicatotActiveColor:'#009ee5'
+    indicatotActiveColor:'#009ee5',
+    cates:[{id:1},{id:2},{id:3},{id:1},{id:5},{id:6},{id:7},{id:8}]
   },
   changeIndicatorDots: function (e) {
     this.setData({
@@ -37,42 +38,60 @@ Page({
     })
   },
   //事件处理函数
-  bindViewTap: function(id) {
-    console.log(id)
+  bindViewTap: function(e) {
+    console.log(e)
     wx.navigateTo({
       url: '../list/list'
     })
   },
+  getIndexData:function(){
+    _this = this
+    wx.request({
+      url: 'http://talent.yoho167.com/api/v1/index', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success:(res)=>{
+        console.log(res.data)
+        _this.setData({
+          cates: res.data.cateList
+        })
+        console.log(_this.cates)
+      }
+    })
+  },
   //监听页面加载
   onLoad: function () {
-    console.log('1111111111')
-    if (app.globalData.userInfo) {
+    
+    this.getIndexData()
+    // console.log('1111111111')
+    // if (app.globalData.userInfo) {
       
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
   },
   getUserInfo: function(e) {
     console.log(e)
