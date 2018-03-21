8d3ae245900e35ba54175ad0977e3a0820492f5a
lyc123 <980469887@qq.com>
2018-03-21 09:34:48 +0800

    完善底部tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -44,57 +44,24 @@ Page({
       url: '../list/list'
     })
   },
-  getIndexData:function(){
+  getIndexData:function(e){
     _this = this
     wx.request({
-      url: 'http://talent.yoho167.com/api/v1/index', //仅为示例，并非真实的接口地址
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
+      url: 'http://talent.yoho167.com/api/v1/index',
       success:(res)=>{
-        console.log(res.data)
-        _this.setData({
-          cates: res.data.cateList
+        this.setData({
+          cates: res.data.data.cateList
         })
-        console.log(_this.cates)
+        console.log(this.data.cates)
       }
     })
   },
   //监听页面加载
   onLoad: function () {
-    
     this.getIndexData()
-    // console.log('1111111111')
-    // if (app.globalData.userInfo) {
-      
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse){
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
   },
   getUserInfo: function(e) {
-    console.log(e)
+    // console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
