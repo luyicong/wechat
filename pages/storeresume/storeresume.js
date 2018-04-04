a883ce0da9a2bbdde02a7068faaa5ae393293c06
lyc123 <980469887@qq.com>
2018-04-04 15:37:06 +0800

    。。。

--- pages/storeresume/storeresume.js
+++ pages/storeresume/storeresume.js
@@ -5,44 +5,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-    resume:{
-      time: '',
-      nowaddress: '',
-      workstate: ''
-    },
+    //姓名
+    realname:'',
+    //性别
+    sex:'',
+    //出生年月日
+    birthy: '',
+    //现居住地
+    nowaddress: '',
+    //目前状态
+    workstate: '',
+    //工作性质
+    jobtype:'',
+    //期待行业
+    industry:'',
+    //目前状态数据
     statusArr:[
       '目前已离职，可快速上岗',
       '目前在职，可考虑换新环境',
       '观望有好的机会再考虑',
       '目前暂无跳槽打算',
       '应届毕业生'
+    ],
+    jobTypeArr:[
+      '全职',
+      '兼职',
+      '实习'
+    ],
+    //性别
+    sexArr:[
+      {
+        name:'男',
+        checked:false,
+        value:'男'
+      },
+      {
+        name: '女',
+        checked: false,
+        value: '女'
+      }
     ]
+  },
+  //姓名
+  realnameChange(e) {
+    console.log(e)
+    this.setData({
+      realname: e.detail.value
+    })
+  },
+  //性别改变
+  sexChange(e) {
+    console.log(e.detail.value)
+    this.setData({
+      sex: e.detail.value
+    })
   },
   //日期选择
   timeChange(e) {
     console.log(e)
     this.setData({
-      resume:{
-        time: e.detail.value
-      }
+      birthy: e.detail.value
     })
   },
   //城市选择
   addressChange(e) {
     console.log(e)
     this.setData({
-      resume:{
         nowaddress: e.detail.value
-      }
     })
   },
   //目前状态改变
   statusChange(e) {
     console.log(e)
     this.setData({
-      resume:{
         workstate: this.data.statusArr[e.detail.value]
-      }
+    })
+  },
+  //工作性质改变
+  jobTypeChange(e) {
+    this.setData({
+      jobtype: e.detail.value
     })
   },
   /**
